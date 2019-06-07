43716d4bdae51d012d9743590769b8d998e80b29
Danyal Ahsanullah <danyal@axiomgc.com>
2019-06-07 12:31:08 -0600

    full api possible to implement implemented

--- src/interface.py
+++ src/interface.py
@@ -1,7 +1,15 @@
 import os
+# only needed for getdict hack
+import numpy as np
+#TODO may wan to define an __all__ thanks to this? or refactor imports
 from ctypes import *
 from enum import Enum
 from atexit import register
+#onle needed for 
+from collections.abc import Sequence
+
+#TODO: DOCUMENTATION!!!
+
 # load DLLs
 # MightexDLL = CDLL('../mightex_lib/x64_lib/Mightex_LEDDriver_SDK.dll')
 # change dir to where DLL is:
@@ -10,16 +18,76 @@ __x86_BASE_PATH = '../mightex_lib/lib/'
 __cdecl_dll = 'Mightex_LEDDriver_SDK.dll'
 __stddel_dll = 'Mightex_LEDDriver_SDK_Stdcall.dll'
 
+__ver_info = (0,1,0)
+__version__ = '.'.join(map(str, __ver_info))
 
-cwd = os.getcwd()
-os.chdir(__x64_BASE_PATH)
-MightexDLL = cdll.LoadLibrary('Mightex_LEDDriver_SDK.dll')
-os.chdir(cwd)
-del cwd
+# util funcs
+def invert_mapping(x):
+    return {v: k for k, v in x.items()}
 
+def get_dll(base_path, dll_type):
+    cwd = os.getcwd()
+    os.chdir(base_path)
+    dll = cdll.LoadLibrary(dll_type)
+    os.chdir(cwd)
+    return dll
+
+# Not used, maybe cut out?
+# https://stackoverflow.com/a/51961249
+def get_shape(lst, shape=()):
+    """
+    returns the shape of nested lists similarly to numpy's shape.
+
+    :param lst: the nested list
+    :param shape: the shape up to the current recursion depth
+    :return: the shape including the current depth
+            (finally this will be the full depth)
+    """
+
+    if not isinstance(lst, Sequence):
+        # base case
+        return shape
+
+    # peek ahead and assure all lists in the next depth
+    # have the same length
+    if isinstance(lst[0], Sequence):
+        l = len(lst[0])
+        if not all(len(item) == l for item in lst):
+            msg = 'not all lists have the same length'
+            raise ValueError(msg)
+
+    shape += (len(lst), )
+
+    # recurse
+    shape = get_shape(lst[0], shape)
+
+    return shape
+
+# HACK: remove this and replace with instance method to convert to dict
+# https://stackoverflow.com/a/3789491
+def getdict(struct):
+    result = {}
+    for field, _ in struct._fields_:
+         value = getattr(struct, field)
+         # if the type is not a primitive and it evaluates to False ...
+         if (type(value) not in [int, float, bool]) and not bool(value):
+             # it's a null pointer
+             value = None
+         elif hasattr(value, "_length_") and hasattr(value, "_type_"):
+             # Probably an array
+             # value = list(value)
+             value = np.ctypeslib.as_array(value).tolist()
+         elif hasattr(value, "_fields_"):
+             # Probably another struct
+             value = getdict(value)
+         result[field] = value
+    return result
+
+
+_MightexDLL = get_dll(__x64_BASE_PATH,__cdecl_dll)
 # constants for the module itself
 SERIAL_NUMBER_SIZE=16
-_ENCODING = 'utf-8'
+_ENCODING = 'ascii'
 
 # vars used internally
 # flag for initialization
@@ -27,8 +95,7 @@ _initialized = False
 
 # will have format (idx:int):(serial_num:str)
 __controller_map = {}
-def invert_mapping(x):
-    return {v: k for k, v in x.items()}
+
 
 #defines 
 MAX_PROFILE_ITEM = 128
@@ -58,16 +125,73 @@ class MightexModuleType(Enum):
 class TLedChannelData(Structure):
     _pack_ = 1
     _fields_ = [
-    ('Normal_CurrentMax',c_int),
-    ('Normal_CurrentSet',c_int),
-    ('Strobe_CurrentMax',c_int),
-    ('Strobe_RepeatCnt',c_int),
-    ('Strobe_Profile',(c_int*MAX_PROFILE_ITEM)*2),
-    ('Trigger_CurrentMax',c_int),
-    ('Trigger_Polarity',c_int),
-    ('Trigger_Profile',(c_int*MAX_PROFILE_ITEM)*2)
+        ('Normal_CurrentMax',c_int),
+        ('Normal_CurrentSet',c_int),
+        ('Strobe_CurrentMax',c_int),
+        ('Strobe_RepeatCnt',c_int),
+        ('Strobe_Profile',(c_int*MAX_PROFILE_ITEM)*2),
+        ('Trigger_CurrentMax',c_int),
+        ('Trigger_Polarity',c_int),
+        ('Trigger_Profile',(c_int*MAX_PROFILE_ITEM)*2)
         ]
 
+    # these don't change so doesnt matter typing keys out instead of pulling
+    _int_keys = {
+        'Normal_CurrentMax', 'Normal_CurrentSet',
+        'Strobe_CurrentMax', 'Strobe_CurrentMax', 'Strobe_RepeatCnt',
+        'Trigger_CurrentMax', 'Trigger_Polarity'
+    }
+    
+    _arr_keys = {
+        'Strobe_Profile','Trigger_Profile'
+    }
+    
+    # _keys = {param[0] for param in _fields_}
+    _keys = _int_keys|_arr_keys
+
+    #TODO: handle strobe and trigger profile size limits
+    #TODO: type checking, all vals are ints
+    #TODO: lookinto accpeting a sequence of 2 element sequences and transposing it
+    @classmethod
+    def from_mapping(cls, mapping):
+        """
+        converts a dict to the struct
+
+        checks to ensure values fit in the struct
+
+        important note: assumes profiles are a sqeuene of of 2 sequences corresponsing to a_n values and b_n values
+        eg: [(a_1,a_2,...,a_n),(b_1,b_2,...,b_n)]
+        """
+        # ensure all keys in mapping are acceptable:
+        assert all([(k in cls._keys) for k in mapping.keys() ])
+        # ensure type checking
+        assert all([isinstance(v,int) for v in [v for k,v in mapping.items() if (k in cls._int_keys)]])
+        assert all([isinstance(m, Sequence) for m in [v for k,v in mapping.items() if (k in cls._arr_keys)]])
+        
+        # not implemented
+        # determine if need to transpose the profile arrays
+        # shape = get_shape()
+
+
+        # transpose and chop after MAX_PROFILE_LEN
+        for k,v in mapping.items():
+            if (k in cls._arr_keys):
+                # mapping[k] = ((c_int*MAX_PROFILE_ITEM)*2)(*list(zip(*v))[:MAX_PROFILE_ITEM-1])
+                mapping[k] = ((c_int*MAX_PROFILE_ITEM)*2)(*tuple([tuple(e[:MAX_PROFILE_ITEM]) for e in v]))
+
+        return cls(**mapping)
+    
+
+    def __str__(self):
+        return "{}: {{{}}}".format(self.__class__.__name__,
+                                ", ".join(["{}: {}".format(field[0],
+                                                            getattr(self,
+                                                                    field[0]))
+                                            for field in self._fields_]))
+
+# print(TLedChannelData._keys)
+
+# TODO: MAYBE annotate the functions? 
 # declare prototypes
 # int MTUSB_LEDDriverInitDevices( void );
 # int MTUSB_LEDDriverOpenDevice( int DeviceIndex );
@@ -87,15 +211,23 @@ class TLedChannelData(Structure):
 # int MTUSB_LEDDriverGetCurrentPara( int DevHandle, int Channel,TLedChannelData *LedChannelDataPtr,int *Mode );
 # int MTUSB_LEDDriverSendCommand( int DevHandle, char *CommandString );
 
+# TODO: look into handling return codes from the functions for confirming or alerting success/failure for api call
+# TODO: convert mostly to properties for things liek channel info for better usage 
 class LEDController:
     def __init__(self, index:int, serial_num=None):
-        self._handle = MightexDLL.MTUSB_LEDDriverOpenDevice(index)
-        self._ch = MightexDLL.MTUSB_LEDDriverDeviceChannels(self._handle)
-        self._device_module_type = MightexDLL.MTUSB_LEDDriverDeviceModuleType(self._handle)
+        self._handle = _MightexDLL.MTUSB_LEDDriverOpenDevice(index)
+        register(self.close)
+        # force into PC mode
+        self.send_cmd('ECHOOFF')
+        self._ch = _MightexDLL.MTUSB_LEDDriverDeviceChannels(self._handle)
+        self._device_module_type = _MightexDLL.MTUSB_LEDDriverDeviceModuleType(self._handle)
         self._ch_info = [[TLedChannelData(),MightexChannelMode.DISABLE_MODE] for i in range(0,self._ch)]
+        # required to enable PC mode on the device, only needs a single call but why not get all at this point
+        for i in range(1,self._ch+1):
+            self.get_ch_param(i)
         if serial_num is None: 
             buf = create_string_buffer(SERIAL_NUMBER_SIZE)
-            MightexDLL.MTUSB_LEDDriverSerialNumber(self._handle, buf, SERIAL_NUMBER_SIZE)
+            _MightexDLL.MTUSB_LEDDriverSerialNumber(self._handle, buf, SERIAL_NUMBER_SIZE)
             self._serial_num = buf.value.decode(_ENCODING)
         else:
             self._serial_num = serial_num
@@ -112,40 +244,63 @@ class LEDController:
     def module_type(self):
         return MightexModuleType(self._device_module_type)
 
+    #TODO: convert to property?
     def get_ch_param(self, channel):
         mode = c_int() 
-        MightexDLL.MTUSB_LEDDriverGetCurrentPara(self._handle, channel, self._ch_info[channel-1][0], byref(mode))
+        _MightexDLL.MTUSB_LEDDriverGetCurrentPara(self._handle, channel, self._ch_info[channel-1][0], byref(mode))
         self._ch_info[channel-1][1] = MightexChannelMode(mode.value) 
 
     def reset_device(self):
-        MightexDLL.MTUSB_LEDDriverResetDevice(self._handle)
+        _MightexDLL.MTUSB_LEDDriverResetDevice(self._handle)
 
-    def set_mode(self,channel:int, mode:MightexChannelMode):
-        MightexDLL.MTUSB_LEDDriverSetMode(self._handle, channel, mode.value)
+    def set_mode(self, channel:int, mode:MightexChannelMode):
+        _MightexDLL.MTUSB_LEDDriverSetMode(self._handle, channel, mode.value)
 
-    def set_current(self,channel:int, current:int):
-        MightexDLL.MTUSB_LEDDriverSetNormalCurrent(self._handle, channel, current)
+    def set_current(self, channel:int, current:int):
+        _MightexDLL.MTUSB_LEDDriverSetNormalCurrent(self._handle, channel, current)
 
     def close(self):
-        MightexDLL.MTUSB_LEDDriverCloseDevice(self._handle)
+        # print('cleaup')
+        _MightexDLL.MTUSB_LEDDriverCloseDevice(self._handle)
+    
+    # note: trigger doesnt exist for some units
+    # note: settingsparameters will change struct, and screen but normal set current will not update 
+    def set_parameters(self, channel:int, parameters:dict):
+        cfg = getdict(self._ch_info[channel-1][0])
+        cfg.update(parameters)
+        params = TLedChannelData.from_mapping(cfg)
+        _MightexDLL.MTUSB_LEDDriverSetNormalPara(self._handle,channel,params)
+        _MightexDLL.MTUSB_LEDDriverSetStrobePara(self._handle,channel,params)
+        _MightexDLL.MTUSB_LEDDriverSetTriggerPara(self._handle,channel,params)
+
+    def restore_default(self):
+        _MightexDLL.MTUSB_LEDDriverRestoreDefault(self._handle)
+
+    def store_parameters(self):
+        _MightexDLL.MTUSB_LEDDriverStorePara(self._handle)
+
+    # note doesnt work for SLC_MAxx-xx models
+    def get_load_voltage(self, channel:int):
+        return _MightexDLL.MTUSB_LEDDriverGetLoadVoltage(self._handle,channel)
 
     def send_cmd(self, command_str):
-        # allocate enough space for string
-        cmd = f'{command_str}\r\n'.encode(_ENCODING)
-        MightexDLL.MTUSB_LEDDriverSendCommand()
+        cmd = f'{command_str}\n\r'.encode(_ENCODING)
+        return _MightexDLL.MTUSB_LEDDriverSendCommand(self._handle, c_char_p(cmd))
 
 # factory function to create a LED controller object
 def get_led_controller(ctrlr_idx=None,serial_num=None): 
     # init api
     if not _initialized:
-        num_devices = MightexDLL.MTUSB_LEDDriverInitDevices()
+        num_devices = _MightexDLL.MTUSB_LEDDriverInitDevices()
         # build controller map
         for i in range(0,num_devices):
-            handle = MightexDLL.MTUSB_LEDDriverOpenDevice(i)
+            handle = _MightexDLL.MTUSB_LEDDriverOpenDevice(i)
+            # send command if needed:
+            # _MightexDLL.MTUSB_LEDDriverSendCommand(handle, c_char_p('ECHOOFF\n\r'.encode(_ENCODING)))
             buf = create_string_buffer(SERIAL_NUMBER_SIZE)
-            MightexDLL.MTUSB_LEDDriverSerialNumber(handle, buf,SERIAL_NUMBER_SIZE)
+            _MightexDLL.MTUSB_LEDDriverSerialNumber(handle, buf,SERIAL_NUMBER_SIZE)
             __controller_map[i] = buf.value.decode(_ENCODING)
-            MightexDLL.MTUSB_LEDDriverCloseDevice(handle)
+            _MightexDLL.MTUSB_LEDDriverCloseDevice(handle)
     # match serial num
     if ctrlr_idx is None and serial_num is not None:
         idx = invert_mapping(__controller_map)[serial_num]
